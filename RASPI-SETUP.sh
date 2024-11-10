19ba35bcaaf4bf77117d1d5ec830b142f2ef3a0f
Noah Frawley <noah.frawley@proton.me>
2024-11-09 23:05:40 -0800

    Let's try this again

--- RASPI-SETUP.sh
+++ RASPI-SETUP.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+
 # Setup wayland so that VNC over rpi-connect is available.
 raspi-config nonint do_wayland W2
 
-# Create Python VENV to isolate
-apt-get install python3 python3-pip
-python -m venv streambot
-source streambot/bin/activate
+# Make sure python3/pip installed... in case of dummies. + chromium-webdriver for the magic
+apt-get install python3 python3-pip chromium-chromedriver
 
-# Install dependencies
-apt-get install chromium-chromedriver
+# Check if venv exists, create if no (It shouldn't on first start)
+if [ ! -d "/venv_streambot"]; then
+    echo "Creating virtual env.."
+    python -m venv /venv_streambot
+else
+    echo "Already there, champ"
+fi
+
+# Activate the venv
+source /venv_streambot/bin/activate
+
+# Install py library dependencies
 pip install selenium python-dotenv webdriver-manager
