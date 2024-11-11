10379f434a29a724a928cd5d76f1299f42b0970b
Noah Frawley <noah.frawley@proton.me>
2024-11-11 15:31:50 -0800

    Update to utilities to include logging, update methods to return dict to handle exceptions in logging

--- utilities.py
+++ utilities.py
@@ -1,9 +1,10 @@
+import logging
 import os, dotenv
-from typing import Optional
+from pathlib import Path
 
 class env:
     @staticmethod
-    def load(file: str, setting: str) -> Optional[str]:
+    def load(file: str, setting: str) -> dict:
         """
         Load the value of a setting from a dotenv file.
 
@@ -12,18 +13,17 @@ class env:
             setting (str): The key of the setting to load.
 
         Returns:
-            str: The value of the setting, or None if an error occurs.
+            dict: Dictionary with keys 'success' and 'result', where 'success' is False if an exception occurs. 'result' contains the value of requested key, or a string of the exception that occurred.
         """
         try:
             dotenv.load_dotenv(file)
             value = os.getenv(setting)
-            return value
+            return {'success': True, 'result': value}
         except Exception as e:
-            print(f"Error loading {setting} from {file}: {e}")
-            return None
+            return {'success': False, 'result': str(e)}
     
     @staticmethod
-    def write(file: str, setting: str, value: str) -> Optional[str]:
+    def write(file: str, setting: str, value: str) -> dict:
         """
         Write a value to a setting in a dotenv file and update the environment variable.
 
@@ -33,19 +33,18 @@ class env:
             value (str): The value to write to the setting.
 
         Returns:
-            str: The value written, or None if an error occurs.
+            dict: Dictionary with keys 'success' and 'result', where 'success' is False if an exception occurs. 'result' contains the value of set key, or a string of the exception that occurred.
         """
         try:
             dotenv.set_key(file, setting, value)
             os.environ[setting] = value
-            return value
+            return {'success': True, 'result': value}
         except Exception as e:
-            print(f"Error writing {value} to {setting} in {file}: {e}")
-            return None
+            return {'success': False, 'result': str(e)}
 
 class files:
     @staticmethod
-    def check_exist(file: str) -> bool:
+    def check_exist(file: str) -> dict:
         """
         Check if a file exists.
 
@@ -53,19 +52,18 @@ class files:
             file (str): The path to the file to check.
 
         Returns:
-            bool: True if the file exists, False otherwise.
+            dict: Dictionary with keys 'success' and 'result', where 'success' is False if an exception occurs. 'result' contains True if file exists, False if the file does not exist, or a string of the exception that occurred.
         """
         try:
             with open(file):
-                return True
+                return {'success': True, 'result': True}
         except FileNotFoundError:
-            return False
+            return {'success': True, 'result': False}
         except Exception as e:
-            print(f"Error checking if file exists {file}: {e}")
-            return False
+            return {'success': False, 'result': str(e)}
     
     @staticmethod
-    def create(file: str) -> bool:
+    def create(file: str) -> dict:
         """
         Create a file and its directories if they do not exist.
 
@@ -73,22 +71,18 @@ class files:
             file (str): The path to the file to create.
 
         Returns:
-            bool: True if the file was created, False otherwise.
+            dict: Dictionary with keys 'success' and 'result', where 'success' is False if an exception occurs. 'result' contains True if file was created, or a string of the exception that occurred.
         """
         try:
             os.makedirs(os.path.dirname(file), exist_ok=True)
             with open(file, 'w') as f:
                 pass
-            return True
-        except OSError as e:
-            print(f"OS error creating file {file}: {e}")
-            return False
+            return {'success': True, 'result': True}
         except Exception as e:
-            print(f"Error creating file {file}: {e}")
-            return False
+            return {'success': False, 'result': str(e)}
     
     @staticmethod
-    def remove(file: str) -> bool:
+    def remove(file: str) -> dict:
         """
         Remove a file.
 
@@ -96,17 +90,53 @@ class files:
             file (str): The path to the file to remove.
 
         Returns:
-            bool: True if the file was removed, False otherwise.
+            dict: Dictionary with keys 'success' and 'result', where 'success' is False if an exception occurs. 'result' contains True if file was removed, False if the file was not found, or a string of the exception that occurred.
         """
         try:
             os.remove(file)
-            return True
+            return {'success': True, 'result': True}
         except FileNotFoundError:
-            print(f"File not found: {file}")
-            return False
-        except OSError as e:
-            print(f"OS error removing file {file}: {e}")
-            return False
+            return {'success': True, 'result': False}
         except Exception as e:
-            print(f"Error removing file {file}: {e}")
-            return False
+            return {'success': False, 'result': str(e)}
+
+class logs:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # If the logging env file does not exist, create it. Default to level INFO.
+        log_settings = str(Path('Settings/logging.env'))
+        if not files.check_exist(log_settings):
+            files.create(log_settings)
+            env.write(log_settings, 'LOGGING_LEVEL', 'INFO')
+        
+        # Load logging settings
+        load_level = env.load(log_settings, 'LOGGING_LEVEL')
+        level = load_level['result']
+        self.app_logger = logging.getLogger(__name__)
+        
+        # Convert string to actual logging level
+        self.level = level.upper() if level else 'INFO'
+        self.app_logger.setLevel(getattr(logging, self.level))
+        print(f"Logging level set to: {level}")
+
+    # Set the logging level if it needs to be updated
+    def set_level(self, level: str):
+        self.new_level = level.upper()
+        self.app_logger.setLevel(getattr(logging, self.new_level))
+        print(f"Logging level updated to {self.new_level}")
+
+    # Print to log file INFO message
+    def info(self, message: str) -> None:
+        self.app_logger.info(f"{__name__} - INFO: {message}")
+    
+    # Print to log file DEBUG message
+    def debug(self, message: str) -> None:
+        self.app_logger.debug(f"{__name__} - DEBUG: {message}")
+
+    # Print to log file CRTICAL message
+    def critical(self, message: str) -> None:
+        self.app_logger.critical(f"{__name__} - CRITICAL: {message}")
+
+    # Print to log file ERROR message
+    def error(self, message: str) -> None:
+        self.app_logger.error(f"{__name__} - ERROR: {message}")
