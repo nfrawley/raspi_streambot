2e97f55deaabc46a61b71f0897fdf9e0d04f10ea
Noah Frawley <noah.frawley@proton.me>
2024-11-11 20:09:02 -0800

    Updated to receive the dict format from utilities.py for more consistent error handling; added logic to choose meeting software and launch with error handling.

--- main.py
+++ main.py
@@ -7,18 +7,53 @@ class App(customtkinter.CTk):
         super().__init__(*args, **kwargs)
         self.title("PyStreamer Bot")
         self.geometry("800x480")
-
+        
+        # Initialize logging
+        self.logs = utilities.logs(app_name="Main-UI")
         # Check if global.env exists, create if not and set defaults
         self.global_env = str(Path('Settings/global.env'))
-        if not utilities.files.check_exist(self.global_env):
-            utilities.env.write(self.global_env, 'SETUP_REQUIRED', 'Yes')
-            utilities.env.write(self.global_env, 'APPEARANCE', 'System')
-            utilities.env.write(self.global_env, 'MEETING_SOFTWARE', 'jitsi')
+        x = utilities.files.check_exist(self.global_env)
+        # If file already exist
+        if x['success'] and x['result']:
+            self.logs.debug(f'File exists: {self.global_env}')
+        # If file is not found, create it
+        elif x['success'] and not x['result']:
+            y = utilities.files.create(self.global_env)
+            # Check if creation was successful
+            match y['success']:
+                # If file was created, add default settings
+                case True: 
+                    self.logs.debug(f"File created: {self.global_env}")
+
+                    settings = {
+                        'SETUP_REQUIRED': 'Yes',
+                        'APPEARANCE': 'System',
+                        'MEETING_SOFTWARE': 'jitsi'
+                    }
+
+                    for key, value in settings.items():
+                        z = utilities.env.write(self.global_env, key, value)
+                        if z['success']:
+                            self.logs.debug(f"Set {key} = {value} in {self.global_env}")
+                        else:
+                            self.logs.error(f"Couldn't write {key} = {value} in {self.global_env}")
+                            self.logs.error(f"{z['result']}")
+                case False: 
+                    self.logs.error(f"Error creating file: {y['result']}")
+        # If there was an exception, log it
+        else:
+            self.logs.error(x['result'])
+
         # Set location for env files for meeting software (Currently only Jitsi, may expand to more)
         self.jitsi_env = str(Path('Settings/jitsi.env'))        
         
         # Load global appearance, set System if errors loading
-        self.default_appearance = utilities.env.load(self.global_env, 'APPEARANCE') or 'System'
+        x = utilities.env.load(self.global_env, 'APPEARANCE')
+        if x['success']:
+            self.logs.debug(f"Loaded {self.global_env}")
+            self.default_appearance = x['result']
+        else:
+            self.logs.critical(x['result'])
 
         # Frame for the options on left side of screen.
         self.sidebar_frame = customtkinter.CTkFrame(self, width=120, corner_radius=0)
@@ -35,39 +70,71 @@ class App(customtkinter.CTk):
         self.sidebar_dropdown_1.set(str(self.default_appearance))
         self.sidebar_dropdown_1.grid(row=3, column=0, padx=20, pady=20)
 
-        
+        # Call init_options to make sure the appropriate files are in place, and fetch options for UI
         self.init_options()
 
 
     def sidebar_button_event(self):
-        print("Sidebar button pressed, functionality TBD")
+        self.logs.debug("Sidebar button pressed, functionality TBD")
         
     def set_appearance(self, new_appearance: str):
+        self.logs.debug("Appearance dropdown menu used")
+        self.logs.debug(f"Setting appearance to: {new_appearance}")
         customtkinter.set_appearance_mode(new_appearance)
-        utilities.env.write(self.global_env, 'APPEARANCE', new_appearance)
-        print(f'Changing appearance to {new_appearance}')
+        x = utilities.env.write(self.global_env, 'APPEARANCE', new_appearance)
+        if x['success']:
+            self.logs.debug(f"Successfully updated to appearance: {new_appearance}")
+        else:
+            self.logs.error(f"Couldn't update appearance in {self.global_env}")
+            self.logs.error(x['result'])
     
     def init_options(self):
+        self.logs.debug(f"Setting appearance: {self.default_appearance}")
         customtkinter.set_appearance_mode(self.default_appearance)
-        if utilities.env.load(self.global_env, 'MEETING_SOFTWARE') == 'JITSI' and not utilities.files.check_exist(self.jitsi_env):
-            utilities.env.write(self.jitsi_env, 'JITSI_SETUP_REQUIRED', 'Yes')
-            utilities.env.write(self.jitsi_env, 'JITSI_URL', 'NONE')
-            utilities.env.write(self.jitsi_env, 'MEETING_ID', 'NONE')
-            utilities.env.write(self.jitsi_env, 'MEETING_URL', 'NONE')
-            utilities.env.write(self.jitsi_env, 'DISPLAY_NAME', 'A Streamer Bot')
-            utilities.env.write(self.jitsi_env, 'USER_NAME', 'NONE')
-            utilities.env.write(self.jitsi_env, 'USER_PASSWORD', 'NONE')
-
-
+        x = utilities.env.load(self.global_env, 'MEETING_SOFTWARE')
+        y = utilities.files.check_exist(self.jitsi_env)
+        
+        # If MEETING_SOFTWARE is jitsi, and the env file does not exist, create it
+        self.logs.debug(f"Software: {x['result']}.")
+        self.logs.debug(f"Env file exists: {y['result']}")
+        if x['result'] == 'jitsi' and y['result'] != True:
+            jitsi_settings = {
+            'JITSI_SETUP_REQUIRED': 'Yes',
+            'JITSI_URL': 'NONE',
+            'MEETING_ID': 'NONE',
+            'DISPLAY_NAME': 'A Streamer Bot',
+            'USER_NAME': 'NONE',
+            'USER_PASSWORD': 'NONE'
+            }
+            # Loop through settings and try to set them in ENV file
+            for key, value in jitsi_settings.items():
+                z = utilities.env.write(self.jitsi_env, key, value)
+                if z['success']:
+                    self.logs.debug(f"Set {key} = {value} in {self.jitsi_env}")
+                else:
+                    self.logs.error(f"Couldn't write {key} = {value} in {self.jitsi_env}")
+                    self.logs.error(f"{z['result']}")
+  
     def join_meeting(self):
-        print('Join Meeting clicked')
-        try:
-            print(f'Trying: {utilities.env.load(self.global_env, 'MEETING_SOFTWARE')} and {utilities.env.load(self.jitsi_env, 'JITSI_SETUP_REQUIRED')}')
-            if utilities.env.load(self.global_env, 'MEETING_SOFTWARE') == 'JITSI' and utilities.env.load(self.jitsi_env, 'JITSI_SETUP_REQUIRED') != 'Yes':
-                print('Starting Jitsi')
-                with sync_playwright() as playwright:
-                    jitsi.run(playwright)
-        except Exception as e:
-            print(f'There was an error joining meeting: {e}')
+        self.logs.debug('Join Meeting clicked')
+        # Check which meeting software should be launched, and if setup is required, before launching
+        x = utilities.env.load(self.global_env, 'MEETING_SOFTWARE')
+        match x['result']:
+            case 'jitsi':
+                self.logs.debug(f"Selected Jitsi Meeting.")
+                y = utilities.env.load(self.jitsi_env, 'JITSI_SETUP_REQUIRED')
+                # If Jitsi setup is not required, attempt to start.
+                if y['result'] != 'Yes':
+                    try:
+                        self.logs.debug("Trying to launch Jitsi meeting...")
+                        with sync_playwright() as playwright:
+                            jitsi.run(playwright)
+                    except Exception as e:
+                        self.logs.error(f"Couldn't launch Jitsi: {e}")
+                else:
+                    self.logs.error("Jitsi requires setup in settings.")
+            case default:
+                self.logs.error(f"There was an error trying to determine meeting software. Are you trying to launch: {x['value']}?")
+                self.logs.critical("You need to set up meeting software settings!")
 
 App().mainloop()
